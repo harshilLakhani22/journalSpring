28cd8439b7ca01f96fbbce227c5f320682453a66
Harshil Lakhani <harshillakhani2264@gmail.com>
2024-10-29 02:30:52 +0530

    use of value annotation

--- src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -5,14 +5,14 @@ import com.example.journalApp.entity.User;
 import com.example.journalApp.repository.JournalEntryRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-@Component
+@Service
 public class JournalEntryService {
 
     @Autowired
